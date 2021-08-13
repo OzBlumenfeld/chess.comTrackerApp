f80cf32fda8d9f6fa59345c55968125c4663aa71
ozb07 <ozb077@gmail.com>
2021-08-13 16:00:02 +0300

    app py

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def email_request():
 #     print('hi there!')
 
 
-# if __name__ == "__main__":
-#     # execute only if run as a script
-#     main()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
