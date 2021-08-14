7583c5c4da9ed24af6c27709e7a786c808d4d9f0
ozb07 <ozb077@gmail.com>
2021-08-14 14:51:46 +0300

    commit

--- db_connection.py
+++ db_connection.py
@@ -1,16 +1,13 @@
 import pymongo
 import os
 
+
 def getDbConnection(db, collection):
-    # file = open("dbCredentials.txt", "r")
-    # credentials = file.read()
     credentials = os.environ['DB_CREDENTIALS']
     print(credentials)
     client = pymongo.MongoClient(credentials)
-
     return client[db][collection]
-    # db = client["AppDB"]
-    # collection =  db["RegisteredUsers"]
+
 
 def findDocument(collection, json):
     return collection.find_one(json)
