ce4cadb67c711e12dc9ae78a8a3275584038e017
ozb07 <ozb077@gmail.com>
2021-09-03 16:18:53 +0300

    Adding stats api

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import db_connection
+
+from chess_utils import get_last_blitz_stats
 
 app = Flask(__name__)
 
@@ -15,11 +17,21 @@ def home_page():
     # return render_template("index.html",
     #     online_users=online_users)
 
+
+# @app.route('/chess')
+
+@app.route("/stats")
+def get_player_stats():
+    user = request.args.get('user')
+    return get_last_blitz_stats(user)
+
+
 @app.route("/")
 def email_request():
-    return 'fuck you !!!'
+    serving = 'serving...'
+    return render_template('app.html', content=serving)
 
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True   , port=5000)
+    app.run(threaded=True, port=5000)
